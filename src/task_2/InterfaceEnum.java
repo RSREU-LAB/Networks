01bf28a5462e3f9fc175f55b3823b02274e9ec35
Кирюха Палыч <thenedafreid@gmail.com>
2025-03-31 15:30:27 +0300

    Add task 2

--- src/task_2/InterfaceEnum.java
+++ src/task_2/InterfaceEnum.java
@@ -3,16 +3,19 @@ package task_2;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
+
 import java.util.Enumeration;
 
 public class InterfaceEnum {
 
     public static void main(String[] args) throws SocketException {
+
         Enumeration<NetworkInterface> ifaces = NetworkInterface.getNetworkInterfaces();
+
         while (ifaces.hasMoreElements()) {
 
             NetworkInterface iface = ifaces.nextElement();
-            System.out.println(iface.getName() + " " + iface.getDisplayName() + ":");
+            System.out.println("\n" + iface.getName() + " " + iface.getDisplayName() + ":");
             Enumeration<InetAddress> addrs = iface.getInetAddresses();
 
             if (!addrs.hasMoreElements()) {
@@ -23,6 +26,36 @@ public class InterfaceEnum {
                 InetAddress addr = addrs.nextElement();
                 System.out.println("\taddress: " + addr.getHostAddress());
             }
+
+            //Вывод MAC адресов
+            printMacAddress(iface);
+        }
+    }
+
+
+    private static void printMacAddress(NetworkInterface iface)
+    {
+        try {
+            // Получаем массив байтов MAC-адреса интерфейса
+            byte[] macAddress = iface.getHardwareAddress();
+
+            if (macAddress != null) {
+                System.out.print("\t MAC address: ");
+                for (int i = 0; i < macAddress.length; i++) {
+                    // Вывод байта 16-ным числом
+                    System.out.format("%02X", macAddress[i]);
+
+                    // Убирается последний дефис
+                    if (i != macAddress.length - 1) {
+                        System.out.print("-");
+                    }
+                }
+                System.out.print("\n");
+            } else {
+                System.out.println("No MAC address");
+            }
+        } catch (SocketException e) {
+            System.err.println("Error getting MAC address: " + e.getMessage());
         }
     }
 }

cc63c0222ccf40a064eeaeb924316c371e97e63c
Кирюха Палыч <thenedafreid@gmail.com>
2025-03-31 16:25:37 +0300

    Add task 3

---
+++ src/task_3/EchoTCPServer.java
@@ -0,0 +1,61 @@
+package task_3;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+public class EchoTCPServer {
+    private static final int BUFSIZE = 256;
+    public static byte[] receiveBuf = new byte[BUFSIZE];
+
+    public static void main(String[] args) throws IOException {
+
+        if (args.length != 1) {
+            throw new IllegalArgumentException("Usage: <Port>");
+        }
+
+        int serverPort = Integer.parseInt(args[0]);
+        var serverSock = new ServerSocket(serverPort);
+
+        System.out.println("Сервер запущен и слушает порт " + serverPort + "...");
+
+        while (true) {
+            Socket clientSock = serverSock.accept();
+            new Thread(() -> connectClient(clientSock)).start();
+        }
+    }
+
+    private static void connectClient(Socket clientSock)
+    {
+        try {
+            SocketAddress clientAddress = clientSock.getRemoteSocketAddress();
+            System.out.println("Accepted client: " + clientAddress);
+
+            InputStream in = clientSock.getInputStream();
+            OutputStream out = clientSock.getOutputStream();
+
+            int recvMsgSize;
+            while ((recvMsgSize = in.read(receiveBuf)) != -1) {
+                out.write(receiveBuf, 0, recvMsgSize);
+            }
+        } catch (IOException e) {
+
+            System.err.println("Error: " + e.getMessage());
+
+        } finally {
+
+            try {
+
+                clientSock.close();
+
+            } catch (IOException ex) {
+
+                System.err.println("Error: " + ex.getMessage());
+
+            }
+        }
+    }
+}

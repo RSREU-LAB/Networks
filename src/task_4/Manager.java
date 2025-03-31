f6ad74dc7a65d1cd7296423b0447a2655d26fff1
Ilya Sokol <innocentbeing911@gmail.com>
2025-03-31 23:52:24 +0300

    Refactor dependencies

--- src/task_4/Manager.java
+++ src/task_4/Manager.java
@@ -3,8 +3,6 @@ package task_4;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
